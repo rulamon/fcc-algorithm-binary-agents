7477e71a8be7446ae12f95f707fe061d165b21c6
rulamon <ruben.lamon@gmail.com>
2018-12-03 22:39:29 +0100

    first solution

---
+++ 1.js
@@ -0,0 +1,12 @@
+const binaryToAscii = (str)=>{
+	let asciiCode = 0;
+	let binaryArr = str.split("");
+	let conversionArr = [128, 64, 32, 16, 8, 4, 2, 1];
+	return binaryArr.map((val, index)=> val * conversionArr[index]).reduce((acc, val)=> acc + val);
+}
+function binaryAgent(str) {
+	let arr = str.split(" ").map(x => binaryToAscii(x)).map(x => String.fromCharCode(x)).join("")
+	return arr;
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
